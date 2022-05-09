9e21c284d04d035264fec9cff3a1ef4249b6d8c6
unknown <rafael.tj18@hotmail.com>
2022-05-09 16:26:50 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,28 @@
+//let nome
+//let idade
+//console.log (typeof nome, typeof idade)
+//undefinid
+
+//const nome =  prompt ("Qual  é o seu nome")
+//const idade =  +prompt ("Qual é a sua idade");
+//console.log (nome, idade)
+//aparece pop-up perguntando o nome e a idade
+
+//let corRoupa = "você está usando roupa azul hoje -";
+//let corPreferida = "Sua cor preferida é azul -";
+//let jogoPreferido = "seu jogo prefeiro é zelda -";
+
+//let resposta1 = "Não";
+//let resposta2 = "Sim";
+//let resposta3 = "Sim";
+//console.log (corRoupa, resposta1 );
+//console.log (corPreferida, resposta2);
+//console.log (jogoPreferido, resposta3);
+
+/*let a = 10;
+let b = 25;
+let c 
+c=a
+a=b
+b=c
+console.log (a,b,c)*/
