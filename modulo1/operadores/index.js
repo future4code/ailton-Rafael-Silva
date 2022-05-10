ab8972e28af52cc49092915ed59d3de3fb8a0546
unknown <rafael.tj18@hotmail.com>
2022-05-10 16:16:33 -0300

    operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,54 @@
+
+//#   EXERCÍCIO DE INTERPRETAÇÃO
+
+// 1#
+// false
+// false
+// true
+// undefined
+
+//2# 
+// vai aparecer os numeros contatenados. 
+// Pois o prompt ler strings e vai impedir a soma.
+// Teria que converter para Number.
+
+//---------EXERCÍCIOS---------
+
+//1#
+
+// const minhaIdade = Number(prompt(""))
+// const idadeDoMeuAmigo = Number(prompt(""))
+// const resultado = minhaIdade === idadeDoMeuAmigo
+// const resultado2 = minhaIdade - idadeDoMeuAmigo
+// console.log("Sua idade é maior do que a do seu melhor amigo?", resultado, resultado2)
+
+//2#
+
+// let valor1 = Number(prompt("digite o primeiro valor"))
+
+// const resultado = valor1 % 2
+// if (resultado == 0){
+//     console.log ("Numero é par")
+// } else {
+//     console.log("Numero é impar")
+// }
+
+//3#
+
+// let minhaIdadeAnos = Number(prompt(""))
+// let resultado = minhaIdadeAnos
+// console.log ("A idade em meses")
+// console.log ("A idade em horas")
+// console.log ("A idade em dias")
+
+//4#
+
+// let valor1 = prompt(" ")
+// let valor2 = prompt(" ")
+//     const resultado = valor1 > valor2
+//     const resultado2 = valor1 === valor2
+//     const resultado3 = valor1 % valor2 === 0
+//     const resultado4 = valor2 % valor1 === 0
+
+// console.log (resultado, resultado2, resultado3, resultado4)
+
