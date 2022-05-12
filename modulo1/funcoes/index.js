76936366b5cfa5d411ede16a2fd1eac254c8db27
unknown <rafael.tj18@hotmail.com>
2022-05-12 15:30:45 -0300

    funcoes complicadas

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,102 @@
+// ==== INTERPRETAÇAO ==================
+
+// 1)
+
+// a) O que vai ser impresso no console?
+// 10, 50
+
+// b) O que aconteceria se retirasse os dois console.log 
+// e simplesmente invocasse a função minhaFuncao(2) e minhaFuncao(10)? O que apareceria no console?
+// R: any
+
+// 2)
+
+// a) Explique o que essa função faz e qual é sua utilidade
+// Ela vai responder false toda palavra que for diferente de "cenoura" em minusculo.
+
+// b) eu gosto de cenoura/ cenoura é bom pra vista/ cenouras crescem na terra
+
+// 1. Escreva as funções explicadas abaixo:
+    
+//     a) A função não deve
+//     receber nenhum parâmetro e deve imprimir uma mensagem falando algumas informações sobre você, como:
+
+// function imprimeNome(nome) {
+//     "Eu sou Rafael, tenho 30 anos, moro em Valparaiso de Goiás e sou estudante."
+// return imprimeNome
+// }
+
+// console.log(imprimeNome)
+
+// b) 
+
+
+// function imprimeNome() {
+//  ` Eu sou ${'Rafael'} tenho ${30} anos, moro em   ${'Valparaiso'} e sou ${'estudante'}
+  
+// `
+// }
+
+// console.log(imprimeNome)
+
+//2
+//a)  
+
+// const a = 10
+// const b = 15
+
+// function  soma (a, b) {
+//     let soma = a + b
+//     return soma 
+// }
+
+// const resultado = soma(a, b)
+// console.log(resultado)
+
+//b)
+
+// const a = 10
+// const b = 15
+
+// function vOuF (a, b){
+//     let vOuF = a >= b
+//     return vOuF
+// }
+
+// const resultado = vOuF(a, b)
+// console.log(resultado)
+
+//c)
+
+// numero = 20
+
+// if(numero % 2 === 0) {
+//     console.log("O número é par");
+// }
+
+//d) 
+
+// let nome1 = 'Jeová Deus Todo Poderoso'
+// function formatar (meuDeus) {
+//     nome1 = meuDeus.toUpperCase();
+
+//     return meuDeus
+// }
+
+// console.log(formatar(nome1))
+
+    //3) 
+
+// const a = 30
+// const b = 15
+
+// function operacoes(a, b){
+// let soma = a + b
+// let subtração = a - b
+// let multiplicação = a * b
+// let divisão = a / b
+
+// return [soma, subtração, multiplicação, divisão]
+// }
+// let resultado = operacoes(a, b)
+// console.log(resultado)
