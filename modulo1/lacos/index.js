3d8ff1539ab6f3c4e179bbea4b5fb8868eb540b9
unknown <rafael.tj18@hotmail.com>
2022-05-18 17:05:07 -0300

    lacos do mau

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,85 @@
+// - **Exercícios de interpretação de código**
+    
+//     1. O que o código abaixo está fazendo? 
+// Qual o resultado impresso no console?
+// Ele está procurando o valor máximo de "1".
+// Res: 10
+
+        // 2. Leia o código abaixo:
+// Vai ser impresso todos os numeros maiores que 18
+// precisa do length. Mas o for... of é suficiente
+
+// 3.Qual seria o resultado 
+// impresso no console, se o usuário digitasse o número 4 ?
+// *
+// **
+// ***
+// ****
+
+// const bichinhosLindos = +prompt("Quantos bichinhos de estimaçao vc tem?")
+// const arr = []
+// if(bichinhosLindos === 0){
+//     console.log("Que pena! Voce pode adotar um pet!")
+// }
+// else{
+//     for (let i=0; i<bichinhosLindos; i++) {
+//         arr.push(prompt(`Qual o nome do seu ${i+1} pet?`))
+// }
+// }
+//  console.log(arr)
+
+// let arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+
+// const funcao1 = () => {
+//         console.log(arrayOriginal)   
+//     } 
+
+// let divisaoDez = () => {
+//     for (let i = 0; i < arrayOriginal.length; i++){
+//         arrayOriginal[i] = arrayOriginal [i]/10}
+//         return arrayOriginal
+// }
+// console.log(divisaoDez())
+
+// //c
+// const newPar = () =>{
+// let novoPar =[]
+
+//     for (let i = 0; i < arrayOriginal.length; i++){
+//         console.log(arrayOriginal[i])
+//         if (arrayOriginal[i] %2===0)
+//         novoPar.push(arrayOriginal[i])       
+//     }       
+// }
+// newPar(arrayOriginal)
+// //d
+// let novoString;
+// const funcao4 = (arr) => {
+//     for (const valor of arr) {
+//         novoString.push(`O elemento do index ${arr.indexOf(valor)} é: ${valor}`)
+//     }
+//     console.log(novoString)
+// }
+
+// //E
+
+// let menorNumero = 0
+// let maiorNumero = 999
+
+// for (const valor of arrayOriginal) {
+//     if (maiorNumero > valor) {
+//         maiorNumero = valor
+//     }
+//     if (menorNumero < valor) {
+//         menorNumero = valor
+//     }
+    
+// }
+// console.log(`O maior número é ${maiorNumero} e o menor número é ${menorNumero}`)
+
+// funcao1(arrayOriginal)
+// divisaoDez(arrayOriginal)
+// newPar(arrayOriginal)
+// funcao4(arrayOriginal)
+
