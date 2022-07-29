9208bccd024eba3463dea6c8a60613ba264fbccb
unknown <rafael.tj18@hotmail.com>
2022-07-29 15:59:32 -0300

    whatslap-reforco

---
+++ modulo3/reforco-projeto-whatslab/whatslab/src/estilizacao/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+width: 40rem;
+height: 95vh;
+border: solid black;
+align-items: stretch;
+display: flex;
+flex-direction: column;
+padding: 20px;
+background-color: #DDF7C8;
+`;
+
+export const FooterWhats = styled.div`
+position: fixed;
+width: 40rem;
+bottom: 0;
+padding: 20px;
+`;
+
+export const WhatslabVerde = styled.h1`
+color: green;
+text-align: center;
+`;
+export const MensagemInput = styled.input`
+width: 100vh;
+`;
