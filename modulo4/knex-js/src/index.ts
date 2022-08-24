ce1822567fe3148bf0e8d081e1d89b9540e77595
rafa-29 <rafael.tj18@hotmail.com>
2022-08-24 19:20:44 -0300

    knex-js

---
+++ modulo4/knex-js/src/index.ts
@@ -0,0 +1,101 @@
+// no index.ts:
+
+import express, { Express } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+export const connection = knex({
+	client: "mysql",
+	connection: {
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA
+  }
+});
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+import { Request, Response } from "express"
+
+// Esse arquivo seria o index.ts
+
+const getActorById = async (id: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE id = '${id}'
+  `)
+
+	return result[0][0]
+}
+
+
+// Assim a chamada funciona fora dos endpoints com .then()/.catch
+getActorById("001")
+	.then(result => {
+		console.log(result)
+	})
+	.catch(err => {
+		console.log(err)
+	});
+
+// Assim a chamada funciona fora dos endpoints com await
+(async () => {
+  console.log(await getActorById("001") )
+})()
+
+
+// Ou então podemos chamá-la dentro de um endpoint
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+
+    console.log(await getActorById(id))
+
+    res.end()
+  } catch (error) {
+
+    res.status(500).send("Unexpected error")
+  }
+}) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
+
+// endpoint pra buscar o ator pelo nome
+app.get("/getUsers", async (req, res) => {
+    try {
+
+        // Usando o query Builder
+        const allUsers = await connection("Actor")
+            .select("name")
+
+        if (!allUsers.length) {
+            res.statusCode = 404
+            throw new Error("Não existe usuarios cadastrados")
+        }
+
+        res.status(200).send(allUsers)
+
+    } catch (error: any) {
+        res.status(res.statusCode || 500).send({ message: error.message })
+    }
+})
+
+
+
+
+
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
