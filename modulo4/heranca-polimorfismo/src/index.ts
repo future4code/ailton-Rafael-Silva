cba414fb1fba043bcccd7846b4c126a9c9784b11
rafa-29 <rafael.tj18@hotmail.com>
2022-09-06 17:34:55 -0300

    heranca-polimorfismo

---
+++ modulo4/heranca-polimorfismo/src/index.ts
@@ -0,0 +1,65 @@
+// no index.ts:
+
+import express, { Express } from "express";
+import knex from "knex";
+import cors from "cors";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+
+dotenv.config();
+
+export const connection = knex({
+	client: "mysql",
+	connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_SCHEMA
+  }
+});
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+class User {
+    private id: string;
+    private email: string;
+    private name: string;
+    private password: string;
+  
+    constructor(
+          id: string,
+          email: string,
+          name: string,
+          password: string
+      ){
+          console.log("Chamando o construtor da classe User")
+          this.id = id
+          this.email = email
+          this.name = name 
+          this.password = password
+      }
+  
+      public getId(): string {
+          return this.id
+      }
+  
+      public getEmail(): string {
+          return this.email
+      }
+  
+      public getName(): string {
+          return this.name
+      }
+
+  }
+
+ const server = app.listen(process.env.PORT || 3003, () => {
+     if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http:localhost: ${address.port}`);
+     } else {
+        console.error(`Failure upon starting server.`);
+     }
+ });
